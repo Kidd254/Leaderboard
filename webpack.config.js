832035c0c31407d98b5eadb3a763c5605914ff8c
Kidd254 <lorenkioko@gmail.com>
2023-06-20 16:56:12 +0300

    fix eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-    mode: 'development',
-   entry: {
-     index: './src/index.js',
-     print: './src/output.js',
-   },
-devtool: 'inline-source-map',
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/output.js',
+  },
+  devtool: 'inline-source-map',
   plugins: [
 
     new HtmlWebpackPlugin({
-template: './src/web.html',
+      template: './src/web.html',
       title: 'Leaderboard',
 
     }),
 
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-      clean: true,
-   },
-   module: {
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
 
     rules: [
 
@@ -38,4 +38,4 @@ template: './src/web.html',
     ],
 
   },
- };
+};
