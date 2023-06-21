b4917a79e9257b7367aca3859f56fc9039100845
Kidd254 <lorenkioko@gmail.com>
2023-06-21 20:01:43 +0300

    modify the refresh function to add recent entry

--- src/module/refresh.js
+++ src/module/refresh.js
@@ -2,10 +2,13 @@ import Scores, { scoreContainer } from './score.js';
 
 const refreshBtn = document.querySelector('.refresh');
 const listScore = new Scores();
+const scoreList = document.createElement('ul');
+scoreList.id = 'score-list';
+scoreContainer.append(scoreList);
 const recentScore = async () => {
   refreshBtn.addEventListener('click', () => {
-    listScore.getList(scoreContainer);
+    listScore.getList(scoreList);
   });
-  listScore.getList(scoreContainer);
+  listScore.getList(scoreList);
 };
 export default recentScore;
