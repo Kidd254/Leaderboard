df8c4a4d93ce634363f591bdfe45cf60cd2ef25a
Kidd254 <lorenkioko@gmail.com>
2023-06-21 18:26:35 +0300

    add function

--- src/module/addScore.js
+++ src/module/addScore.js
@@ -1,18 +1,24 @@
-import { display } from './score.js';
+import Scores from './score.js';
 
 const nameInput = document.querySelector('#name');
-const scoreInput = document.querySelector('#score');
 export const formElement = document.querySelector('#form');
-export const addScore = (event) => {
-  // If the inputs has values then form is submitted
-  if (!nameInput.validity.valueMissing && !scoreInput.validity.valueMissing) {
-    event.preventDefault();
-    const scored = { name: nameInput.value, score: scoreInput.value };
-    // Save the name and score in the array and the local storage
-    display.storage(scored);
-    // Resets the form
-    formElement.reset();
-    // Creates the new scores element
-    display.create(scored);
-  }
+const scoreInput = document.querySelector('#score');
+const submitBtn = document.querySelector('.submit');
+const addYourScore = () => {
+  const listScore = new Scores();
+  submitBtn.addEventListener('click', (event) => {
+    if (scoreInput.value === '' || nameInput.value === '') {
+      event.preventDefault();
+    }
+    if (!Number.isNaN(parseInt(scoreInput.value, 10))) {
+      const scored = { name: nameInput.value, score: scoreInput.value };
+      // Save the name and score in the array and the local storage
+      listScore.storage(scored);
+      formElement.reset();
+      // Creates the new scores element
+      listScore.create(scored);
+    }
+  });
 };
+
+export default addYourScore;
