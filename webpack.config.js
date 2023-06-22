60b9ab14227de46d55488fda085e27354c62ee43
Kidd254 <lorenkioko@gmail.com>
2023-06-22 16:20:36 +0300

    configure webpack.config.js to accomodate added fonts

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,13 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
 
       },
+      {
+
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: 'asset/resource',
+
+      },
 
     ],
 
