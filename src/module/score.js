9c564b3c949bc4bcf3c709d4e9cc25d7f3f26d82
Kidd254 <lorenkioko@gmail.com>
2023-08-12 15:13:31 +0300

    modify scores container for appeal

--- src/module/score.js
+++ src/module/score.js
@@ -8,52 +8,57 @@ export default class Scores {
     }
   }
 
-    storage=(score) => {
-      this.scoreList.push(score);
-      localStorage.setItem('info', JSON.stringify(this.scoreList));
-    }
+  storage = (score) => {
+    this.scoreList.push(score);
+    localStorage.setItem('info', JSON.stringify(this.scoreList));
+  };
 
-    create=(scoreContainer) => {
-      scoreContainer.innerHTML = '';
-      this.scoreList.forEach((score) => {
-        const scoreListContainer = document.createElement('div');
-        scoreListContainer.classList.add('list-container');
-        const name = document.createElement('p');
-        name.classList.add('name-added');
-        const points = document.createElement('p');
-        points.classList.add('score-added');
+  create = (scoreContainer) => {
+    scoreContainer.innerHTML = '';
+    this.scoreList.forEach((score) => {
+      const scoreListContainer = document.createElement('div');
+      scoreListContainer.classList.add('list-container');
+      const name = document.createElement('p');
+      name.classList.add('name-added');
+      const points = document.createElement('p');
+      points.classList.add('score-added');
 
-        name.innerHTML = score.user;
-        points.innerHTML = score.score;
+      name.innerHTML = score.user;
+      points.innerHTML = `: ${score.score}`;
 
-        scoreListContainer.append(name);
-        scoreListContainer.append(points);
-        scoreContainer.append(scoreListContainer);
-      });
-      return scoreContainer;
-    }
+      scoreListContainer.append(name);
+      scoreListContainer.append(points);
+      scoreContainer.append(scoreListContainer);
+    });
+    return scoreContainer;
+  };
 
-     addScores = (userData, scoreData) => {
-       fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RPILbm8HelgXKYt5Q7oK/scores/', {
-         method: 'POST',
-         body: JSON.stringify({
-           user: userData,
-           score: scoreData,
-         }),
-         headers: {
-           'Content-type': 'application/json; charset=UTF-8',
-         },
-       });
-     }
+  addScores = (userData, scoreData) => {
+    fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RPILbm8HelgXKYt5Q7oK/scores/',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          user: userData,
+          score: scoreData,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      },
+    );
+  };
 
-     async getList(scoreContainer) {
-       const fetchData = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RPILbm8HelgXKYt5Q7oK/scores/');
-       this.fetching(fetchData, scoreContainer);
-     }
+  async getList(scoreContainer) {
+    const fetchData = await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/RPILbm8HelgXKYt5Q7oK/scores/',
+    );
+    this.fetching(fetchData, scoreContainer);
+  }
 
-     async fetching(fetchData, scoreContainer) {
-       const toJson = await fetchData.json();
-       this.scoreList = toJson.result;
-       this.create(scoreContainer);
-     }
+  async fetching(fetchData, scoreContainer) {
+    const toJson = await fetchData.json();
+    this.scoreList = toJson.result;
+    this.create(scoreContainer);
+  }
 }
